3835d70bcce63dafa0e1c4665b882a5b4df57df4
tunggvu <tunggvu@gmail.com>
2018-04-19 04:30:17 +0000

    first commit

---
+++ app/controllers/pages/info_controller.rb
@@ -0,0 +1,2 @@
+class Pages::InfoController < ApplicationController
+end
---
+++ test/controllers/pages/info_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Pages::InfoControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
